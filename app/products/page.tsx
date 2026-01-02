def1ecfa8ab36b08ddede577963f498901c40b98
yohannis <Abayyohannis2014@gmail.com>
2026-01-02 09:07:30 +0300

    d

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { ProductCard } from '@/components/product-card'
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
